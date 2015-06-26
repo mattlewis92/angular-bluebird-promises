cbd44c47ecba6af835c6ff54c3fdb376268c0fce
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-26 15:20:47 +0100

    Build 0.4.0

--- dist/angular-bluebird-promises.min.js
+++ dist/angular-bluebird-promises.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-bluebird-promises - Replaces $q with bluebirds promise API
- * @version v0.3.6
+ * @version v0.4.0
  * @link https://github.com/mattlewis92/angular-bluebird-promises
  * @license MIT
  */
