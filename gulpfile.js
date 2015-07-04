50624d8431a27ff60693fa0b88648498db012a82
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-04 17:40:57 +0100

    Use webpack for running the livereload server

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,24 @@
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
 var open = require('open');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var webpackHelper = require('./webpackHelper');
 
-gulp.task('watch', ['server'], function() {
-  $.livereload.listen();
-  gulp.watch('src/*.js', ['build:dev']);
-  gulp.watch(['index.html', 'dist/angular-bluebird-promises.js']).on('change', $.livereload.changed);
-});
-
 gulp.task('server', function() {
   var PORT = 8000;
   var HOST = 'localhost';
-  new WebpackDevServer(webpack(webpackHelper.getDevConfig())).listen(PORT, HOST);
+  var config = webpackHelper.getDevConfig();
+  delete config.externals;
+  new WebpackDevServer(webpack(config)).listen(PORT, HOST);
   open('http://' + HOST + ':' + PORT);
 });
 
 function webpackCb(done) {
   return function(err, stats) {
-    $.util.log('[webpack]', stats.toString());
+    console.log('[webpack]', stats.toString());
     for (var file in stats.compilation.assets) {
       if (!stats.compilation.assets[file].emitted) {
-        return done(new Error(file + ' was not build as there was an error!'));
+        return done(new Error(file + ' was not built as there was an error!'));
       }
     }
   }
@@ -37,4 +32,4 @@ gulp.task('build:prod', function(done) {
   webpack(webpackHelper.getProdConfig(), webpackCb(done));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['server']);
