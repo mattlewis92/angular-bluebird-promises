c96140e7fb96b2cb065eb5358cc61700499ce827
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-04 18:26:16 +0100

    Use separate webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 var open = require('open');
 var karma = require('karma');
 var webpackHelper = require('./webpackHelper');
-var config = process.argv.indexOf('--min') > -1 ? webpackHelper.getMinConfig() : webpackHelper.getUnminConfig();
-var isServer = process.env._.indexOf('webpack-dev-server') > -1;
-if (isServer) {
-  delete config.externals;
-  open('http://localhost:' + 8000);
-  karma.server.start({
-    configFile: __dirname + '/karma.conf.js',
-    autoWatch: true,
-    singleRun: false
-  });
-}
+var config = webpackHelper.getUnminConfig();
+delete config.externals;
+open('http://localhost:' + 8000);
+karma.server.start({
+  configFile: __dirname + '/karma.conf.js',
+  autoWatch: true,
+  singleRun: false
+});
 module.exports = config;
