dfaf2aa753675ea7877e1129fa2cb2a6bfd877a3
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-04 18:02:46 +0100

    Auto run karma tests when starting the server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 var open = require('open');
+var karma = require('karma');
 var webpackHelper = require('./webpackHelper');
 var config = process.argv.indexOf('--prod') > -1 ? webpackHelper.getProdConfig() : webpackHelper.getDevConfig();
 var isServer = process.env._.indexOf('webpack-dev-server') > -1;
 if (isServer) {
   delete config.externals;
   open('http://localhost:' + 8000);
+  karma.server.start({
+    configFile: __dirname + '/karma.conf.js',
+    autoWatch: true,
+    singleRun: false
+  });
 }
 module.exports = config;
