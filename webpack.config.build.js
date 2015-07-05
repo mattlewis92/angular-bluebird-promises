f21a2f7499503f100df1d771e212eb2681592969
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-05 10:23:04 +0100

    Directly write the webpack config into files rather than using the helper

---
+++ webpack.config.build.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var ejs = require('ejs');
+
+function getBanner() {
+  var pkg = require('./bower.json');
+  var banner = ['/**',
+    ' * <%= pkg.name %> - <%= pkg.description %>',
+    ' * @version v<%= pkg.version %>',
+    ' * @link <%= pkg.homepage %>',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    ''].join('\n');
+  return ejs.render(banner, {pkg: pkg});
+}
+
+module.exports = {
+  entry: __dirname + '/src/angular-bluebird-promises.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: process.argv.indexOf('-p') > -1 ? 'angular-bluebird-promises.min.js' : 'angular-bluebird-promises.js'
+  },
+  externals: {
+    angular: 'angular',
+    bluebird: 'Promise'
+  },
+  devtool: 'source-map',
+  module: {
+    preLoaders: [{
+      test: /.*\.js$/,
+      loaders: ['eslint'],
+      exclude: /node_modules/
+    }],
+    loaders: [{
+      test: /.*\.js$/,
+      loaders: ['ng-annotate'],
+      exclude: /node_modules/
+    }]
+  },
+  eslint: {
+    configFile: __dirname + '/.eslintrc'
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.BannerPlugin(getBanner(), {
+      raw: true,
+      entryOnly: true
+    })
+  ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 var open = require('open');
 var karma = require('karma');
-var webpackHelper = require('./webpackHelper');
-var config = webpackHelper.getUnminConfig();
-delete config.externals;
+
 open('http://localhost:' + 8000);
 karma.server.start({
   configFile: __dirname + '/karma.conf.js',
   autoWatch: true,
   singleRun: false
 });
-module.exports = config;
+
+module.exports = {
+  entry: __dirname + '/src/angular-bluebird-promises.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'angular-bluebird-promises.js'
+  },
+  module: {
+    preLoaders: [{
+      test: /.*\.js$/,
+      loaders: ['eslint'],
+      exclude: /node_modules/
+    }]
+  },
+  eslint: {
+    configFile: __dirname + '/.eslintrc'
+  }
+};
