dd084bf718b5efced2f56241003f97dc30e78ef0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-07 13:21:49 +0100

    Release 0.5.1

--- dist/angular-bluebird-promises.js
+++ dist/angular-bluebird-promises.js
@@ -1,48 +1,58 @@
 /**
  * angular-bluebird-promises - Replaces $q with bluebirds promise API
- * @version v0.5.0
+ * @version v0.5.1
  * @link https://github.com/mattlewis92/angular-bluebird-promises
  * @license MIT
  */
 
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("angular"), require("Promise"));
+	else if(typeof define === 'function' && define.amd)
+		define(["angular", "Promise"], factory);
+	else {
+		var a = typeof exports === 'object' ? factory(require("angular"), require("Promise")) : factory(root["angular"], root["Promise"]);
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(this, function(__WEBPACK_EXTERNAL_MODULE_1__, __WEBPACK_EXTERNAL_MODULE_2__) {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -52,19 +62,19 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-	
+
 	var angular = __webpack_require__(1);
 	var bluebird = __webpack_require__(2);
 	var MODULE_NAME = 'mwl.bluebird';
-	
+
 	angular
 	  .module(MODULE_NAME, [])
 	  .constant('Bluebird', bluebird)
 	  .config(["$provide", "Bluebird", function($provide, Bluebird) {
-	
+
 	    //Make bluebird API compatible with angular's subset of Q
 	    //Adapted from: https://gist.github.com/petkaantonov/8363789 and https://github.com/petkaantonov/bluebird-q
-	
+
 	    Bluebird.defer = function() {
 	      var b = Bluebird.pending();
 	      b.resolve = angular.bind(b, b.fulfill);
@@ -72,41 +82,41 @@
 	      b.notify = angular.bind(b, b.progress);
 	      return b;
 	    };
-	
+
 	    Bluebird.reject = Bluebird.rejected;
 	    Bluebird.when = Bluebird.cast;
-	
+
 	    var originalAll = Bluebird.all;
 	    Bluebird.all = function(promises) {
-	
+
 	      if (angular.isObject(promises) && !angular.isArray(promises)) {
 	        return Bluebird.props(promises);
 	      } else {
 	        return originalAll.call(Bluebird, promises);
 	      }
-	
+
 	    };
-	
+
 	    var originalFinally = Bluebird.prototype.finally;
 	    Bluebird.prototype.finally = function(finallyCallback, progressCallback) {
 	      this.progressed(progressCallback);
 	      return originalFinally.call(this, finallyCallback);
 	    };
-	
+
 	    Bluebird.onPossiblyUnhandledRejection(angular.noop);
-	
+
 	    $provide.decorator('$q', function() {
 	      return Bluebird;
 	    });
-	
+
 	  }]).run(["$rootScope", "Bluebird", function($rootScope, Bluebird) {
-	
+
 	    Bluebird.setScheduler(function(cb) {
 	      $rootScope.$evalAsync(cb);
 	    });
-	
+
 	  }]);
-	
+
 	module.exports = MODULE_NAME;
 
 
@@ -114,14 +124,15 @@
 /* 1 */
 /***/ function(module, exports) {
 
-	module.exports = angular;
+	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
 
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
 
-	module.exports = Promise;
+	module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
 
 /***/ }
-/******/ ]);
-//# sourceMappingURL=angular-bluebird-promises.js.map
+/******/ ])
+});
+;
