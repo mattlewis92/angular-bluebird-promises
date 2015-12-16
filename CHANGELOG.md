ad5f565beea298b21421b9119f57423151482091
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-12-16 17:29:29 +0000

    chore(release): 1.0.0 🌟

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+<a name="1.0.0"></a>
+# [1.0.0](https://github.com/mattlewis92/angular-bluebird-promises/compare/0.6.4...v1.0.0) (2015-12-16)
+
+* Stable API
--- dist/angular-bluebird-promises.min.js
+++ dist/angular-bluebird-promises.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-bluebird-promises - Replaces $q with bluebirds promise API
- * @version v0.6.4
+ * @version v1.0.0
  * @link https://github.com/mattlewis92/angular-bluebird-promises
  * @license MIT
  */
